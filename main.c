764aa70fd35b555467e96ee6422b169b6db91dff
selabnayr <ryanb@stmproducts.com>
2019-10-21 17:33:31 -0700

    Can't do that... Can we go faster?

--- main.c
+++ main.c
@@ -152,6 +152,9 @@ int main(void)
   Fx3GctlInitClock();
   *(volatile uint32_t *)(void *)0x400020e8 = 0;
   Fx3GctlInitIoMatrix(FX3_GCTL_ALTFUNC_GPIF32BIT_UART_I2S);
+  Fx3UartInit(1152000, FX3_UART_NO_PARITY, FX3_UART_1_STOP_BIT);
+  Fx3UartTxString("\nGood moaning!\n");
+  Fx3UartTxFlush();
 
   Fx3GpioInitClock();
   Fx3GpioSetupSimple(45,
@@ -177,10 +180,6 @@ int main(void)
 
   Fx3UsbInit(&callbacks);
   Fx3UsbConnect();
-
-  Fx3UartInit(115200, FX3_UART_NO_PARITY, FX3_UART_1_STOP_BIT);
-  Fx3UartTxString("\nGood moaning!\n");
-  Fx3UartTxFlush();
 
   for(;;) {
     poll_acquisition();
